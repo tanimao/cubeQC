ebb3c28dbbf0203abbe8d3e1679c1a18372e6769
tanimao <tani.mao.75w@st.kyoto-u.ac.jp>
2020-10-07 15:52:20 +0900

    201007キューブ検査

--- ChangeRun.sh
+++ ChangeRun.sh
@@ -2,31 +2,31 @@
 
 
 
-sed -e "s%pic200930%pic201005%" ./cubeQC0827.py > tmp11
-sed -e "s%data200930_%data201005_%" ./tmp11 > tmp1
+sed -e "s%pic201005%pic201007%" ./cubeQC0827.py > tmp11
+sed -e "s%data201005_%data201007_%" ./tmp11 > tmp1
 
 mv tmp1 cubeQC0827.py
 
 
 
 
-sed -e "s%pic200930%pic201005%" ./QCpackage2/camera0814.py > tmp2
+sed -e "s%pic201005%pic201007%" ./QCpackage2/camera0814.py > tmp2
 
 mv tmp2 QCpackage2/camera0814.py
 
 
 
 
-sed -e "s%data200930_%data201005_%" ./QCpackage2/MainAnalysis0604.py > tmp3
+sed -e "s%data201005_%data201007_%" ./QCpackage2/MainAnalysis0604.py > tmp3
 
 mv tmp3 QCpackage2/MainAnalysis0604.py
 
 
 
-sed -e "s%data200930_cali%data201005_cali%" ./QCpackage2/checkSign.py > tmp4
+sed -e "s%data201005_cali%data201007_cali%" ./QCpackage2/checkSign.py > tmp4
 
 mv tmp4 QCpackage2/checkSign.py
 
 
 
-mkdir mao_pictures/pic201005/
+mkdir mao_pictures/pic201007/
